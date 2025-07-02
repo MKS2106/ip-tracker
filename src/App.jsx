4c5a64192bea6e6753d07fbfa766e6307a2f175a
Manasa <manasa.7m@gmail.com>
2025-07-02 01:07:05 -0400

    Added background images for styling as per requirement

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,79 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import { useParams } from "react-router-dom";
-import useFetch from './hooks/useFetch';
-import Map from './components/Map';
+import { useState } from "react";
+import "./App.css";
+import useFetch from "./hooks/useFetch";
+import Map from "./components/Map";
 
 const API_KEY = import.meta.env.VITE_IPTRACKER_API_KEY;
 
-
 function App() {
-
-  const [searchIp, setSearchIp] = useState("")
-  const [newIp, setNewIp] = useState("")
-  const apiUrl= `https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}&ipAddress=${newIp}`
-  const { data, loading, error } = useFetch( apiUrl
-    );
-   const handleSubmit= (e)=> {
+  const [searchIp, setSearchIp] = useState("");
+  const [newIp, setNewIp] = useState("");
+  // const apiUrl = `https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}&ipAddress=${newIp}`;
+  const apiUrl = `https://geo.ipify.org/api/v2/country,city,vpn?apiKey=${API_KEY}&ipAddress=${newIp}`;
+  const { data, loading, error } = useFetch(apiUrl);
+  const handleSubmit = (e) => {
     e.preventDefault();
-    setNewIp(searchIp)
-    // setSearchIp("");
+    setNewIp(searchIp);
+    setSearchIp("");
+  };
+  //  const value = data?.ip[0];
+  if (error) {
+    return (
+      <h2>
+        {error.message.includes("Invalid")
+          ? "Invalid IP Address. Please enter a valid IP address."
+          : error.message}
+      </h2>
+    );
   }
- if (error) return <h2>Error Message</h2>;
-   const value = data?.ip[0];
 
-  if (loading || !value)
+  if (loading || !data)
     return <h2 className="text-5xl animate-pulse">loading...</h2>;
- const lat = data.location.lat;
- const lng = data.location.lng;
- console.log({searchIp})
+  const lat = data?.location?.lat;
+  const lng = data?.location?.lng;
+  console.log({ searchIp });
 
   return (
-    <>
-      <h1>IP Tracker</h1>
-      <form onSubmit={handleSubmit}>
-       <input
-          className="border focus:ring-green-300 rounded w-1/2 px-3 py-1 mr-4"
-          type="text"
-          value={searchIp}
-          placeholder="Search Address"
-          onChange={(e) => setSearchIp(e.target.value)}         
-        />
-        <button type = "submit" className="rounded bg-green-300 font-semibold px-4 py-1 rounded hover:bg-green-600">Search</button>
-    </form>
     <div>
-        <h2>{data.location?.city}</h2>
-        <p>{data.location?.region}</p>
-        <p>{data.location?.time}</p>
-       <p>{data.ip}</p>
-    </div>
-    <Map lat={lat} lng={lng}></Map>
+      <div className="relative">
     
-    </>
-  )
+      <div className="bg-[url(./assets/pattern-bg-desktop.png)] py-8 px-4 border">
+        <h1 className="text-3xl font-semibold text-center mb-6 text-white">
+          IP Address Tracker
+        </h1>
+        <form
+          className="flex justify-center items-center mb-12 flex-wrap"
+          onSubmit={handleSubmit}
+        >
+          <input
+            className="w-full sm:w-1/2 px-4 py-2 bg-white text-gray-900 rounded-lg border border-gray-300 focus:ring-2 focus:ring-blue-400  shadow-sm"
+            type="text"
+            value={searchIp}
+            placeholder="Search IP Address or domain"
+            onChange={(e) => setSearchIp(e.target.value)}
+          />
+          <button
+            type="submit"
+            className="rounded bg-black px-4 py-1"
+          >
+            <img src="/icon-arrow.svg" alt="search" className="w-8 h-8"/>
+          </button>
+        </form>
+      </div>
+        <div className="absolute top-full left-1/2 -translate-x-1/2 -translate-y-1/2 z-10 w-[90%] max-w-5xl bg-white rounded-lg shadow-xl p-6 flex flex-wrap justify-between text-gray-800">
+            
+            <p><span className="font-medium text-gray-900">IP Address: <br/></span> {data.ip}</p>
+            <p ><span className="font-medium text-gray-900">Location: <br/></span>{data.location?.city},{data.location?.region},<br/>{data.location?.postalcode}</p>
+            <p><span className="font-medium text-gray-900">TimeZone: <br/></span>UTC{data.location?.timezone}</p>
+            <p><span className="font-medium text-gray-900">ISP: <br/></span>{data?.isp}</p>
+            
+         </div>
+      </div>
+      <div className="w-full relative z-0">
+        <Map lat={lat} lng={lng}></Map>
+      </div>
+      </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,8 +1,9 @@
-import "leaflet/dist/leaflet.css";
+import "leaflet/dist/leaflet.css";//importing leaflet 
 import { useEffect } from "react";
-import { MapContainer, TileLayer, useMap } from "react-leaflet";
+import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet"; //importing components from leaflet
 
-export function MapUpdater(lat, lng){
+//component to set/update the latitude and longitude when user search for an ip address
+export function MapUpdater({lat, lng}){
   const map = useMap();
   useEffect(() => {
     map.setView([lat, lng], 13);
@@ -10,16 +11,26 @@ export function MapUpdater(lat, lng){
   return null;
 }
 
+// function to render map using leaflet
 function Map({ lat, lng }) {
   return (
+    <div className="w-full h-[60vh] sm:h-[400px] rounded shadow-lg border border-gray-300 ">
     <MapContainer
+      key={`${lat}-${lng}`}
       center={[lat, lng]}
       zoom={13}
-      style={{ height: "400px", width: "100%" }}
+      className="w-full h-full"
+      scrollWheelZoom={true}
     >
       <TileLayer url="https://tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      MapUpdater(lat,lng)
+      {/* a pin showing the location based on the ip address */}
+      <Marker position={[lat, lng]}>
+        {/* a pop up to show on the map when the pin/marker is clicked */}
+        <Popup>IP Location</Popup> 
+      </Marker>
+      <MapUpdater lat={lat} lng={lng}/>
     </MapContainer>
+    </div>
   );
 }
 export default Map;
