8ff33ced081e74d40cb76a1ac743f7bedd5291fa
Manasa <manasa.7m@gmail.com>
2025-07-02 23:06:33 -0400

    Added Footer

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ const API_KEY = import.meta.env.VITE_IPTRACKER_API_KEY;
 function App() {
   const [searchIp, setSearchIp] = useState("");
   const [newIp, setNewIp] = useState("");
-  // const apiUrl = `https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}&ipAddress=${newIp}`;
   const apiUrl = `https://geo.ipify.org/api/v2/country,city,vpn?apiKey=${API_KEY}&ipAddress=${newIp}`;
   const { data, loading, error } = useFetch(apiUrl);
   const handleSubmit = (e) => {
@@ -18,7 +17,7 @@ function App() {
   };
   //  const value = data?.ip[0];
   if (error) {
-    console.log(error.message)
+    console.log(error.message);
     return (
       <h2 className="text-2xl font-semibold text-center whitespace-pre-line">
         {error.message.includes("Invalid")
@@ -37,44 +36,65 @@ function App() {
   return (
     <div>
       <div className="relative">
-    
-      <div className="bg-[url(./assets/pattern-bg-desktop.png)] py-8 px-4 border">
-        <h1 className="text-3xl font-semibold text-center mb-6 text-white">
-          IP Address Tracker
-        </h1>
-        <form
-          className="flex justify-center items-center mb-12 flex-wrap"
-          onSubmit={handleSubmit}
-        >
-          <input
-            className="w-full sm:w-1/2 px-4 py-2 bg-white text-gray-900 rounded-lg border border-gray-300 focus:ring-2 focus:ring-blue-400  shadow-sm"
-            type="text"
-            value={searchIp}
-            placeholder="Search IP Address or domain"
-            onChange={(e) => setSearchIp(e.target.value)}
-          />
-          <button
-            type="submit"
-            className="rounded bg-black px-4 py-1"
+        <div className="bg-[url(./assets/pattern-bg-desktop.png)] py-8 px-4 border">
+          <h1 className="text-3xl font-semibold text-center mb-6 text-white">
+            IP Address Tracker
+          </h1>
+          <form
+            className="flex justify-center items-center mb-12 flex-wrap"
+            onSubmit={handleSubmit}
           >
-            <img src="/icon-arrow.svg" alt="search" className="w-8 h-8"/>
-          </button>
-        </form>
-
-      </div>
+            <input
+              className="w-full sm:w-1/2 px-4 py-2 bg-white text-gray-900 rounded-lg border border-gray-300 focus:ring-2 focus:ring-blue-400  shadow-sm"
+              type="text"
+              value={searchIp}
+              placeholder="Search IP Address or domain"
+              onChange={(e) => setSearchIp(e.target.value)}
+            />
+            <button type="submit" className="rounded bg-black px-4 py-1">
+              <img src="/icon-arrow.svg" alt="search" className="w-8 h-8" />
+            </button>
+          </form>
+        </div>
         <div className="absolute top-full left-1/2 -translate-x-1/2 -translate-y-1/2 z-10 w-[90%] max-w-5xl bg-white rounded-lg shadow-xl p-6 flex flex-wrap justify-between text-gray-800">
-            
-            <p><span className="font-medium text-gray-900">IP Address: <br/></span> {data.ip}</p>
-            <p ><span className="font-medium text-gray-900">Location: <br/></span>{data.location?.city},{data.location?.region},<br/>{data.location?.postalcode}</p>
-            <p><span className="font-medium text-gray-900">TimeZone: <br/></span>UTC{data.location?.timezone}</p>
-            <p><span className="font-medium text-gray-900">ISP: <br/></span>{data?.isp}</p>
-            
-         </div>
+          <p>
+            <span className="font-medium text-gray-900">
+              IP Address: <br />
+            </span>{" "}
+            {data.ip}
+          </p>
+          <p>
+            <span className="font-medium text-gray-900">
+              Location: <br />
+            </span>
+            {data.location?.city},{data.location?.region},<br />
+            {data.location?.postalcode}
+          </p>
+          <p>
+            <span className="font-medium text-gray-900">
+              TimeZone: <br />
+            </span>
+            UTC{data.location?.timezone}
+          </p>
+          <p>
+            <span className="font-medium text-gray-900">
+              ISP: <br />
+            </span>
+            {data?.isp}
+          </p>
+        </div>
       </div>
       <div className="w-full relative z-0">
         <Map lat={lat} lng={lng}></Map>
       </div>
-      </div>
+
+      <footer className="text-center font-semibold py-4">
+        <p>
+          Copyright &#169; 2025 IP Address Tracker: Developed by- Manasa
+          Madhihalli Swamy
+        </p>
+      </footer>
+    </div>
   );
 }
 
